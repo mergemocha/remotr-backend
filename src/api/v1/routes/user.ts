8d75e0b569ea4757ea77f7bcb7d2b4eb3ef5a472
Anna Smati <anna.smati@gmail.com>
2021-04-27 17:40:15 +0300

    Added initial login authentication function

--- src/api/v1/routes/user.ts
+++ src/api/v1/routes/user.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express'
+import { internalServerError, ok } from '../../../utils/cannedHTTPResponses'
 
 const router = Router()
 
 // TODO: Auth checks
 
-router.post('/login', (req, res, next) => {
-  // TODO
-  res.sendStatus(200)
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.post('/login', async (req, res, next) => {
+  try {
+    if (req.body.username === process.env.LOGIN_USER && req.body.password === process.env.LOGIN_PASS) {
+      logger.info('User authenticated. Login successful.')
+      ok(res)
+    } else {
+      logger.warn('User input invalid. Login unsuccesful.')
+      res.sendStatus(400)
+    }
+  } catch (err) {
+    logger.warn('Error during login.')
+    internalServerError(res)
+  }
 })
 
 router.post('/auth', (req, res, next) => {
