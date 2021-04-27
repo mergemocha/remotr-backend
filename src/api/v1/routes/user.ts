6f7a0106114bb5d2f223d3e406efdf601e0f72de
Anna Smati <anna.smati@gmail.com>
2021-04-28 00:45:49 +0300

    Improved log messages

--- src/api/v1/routes/user.ts
+++ src/api/v1/routes/user.ts
@@ -1,11 +1,9 @@
 import crypto from 'crypto'
-import { Router } from 'express'
-import { internalServerError, ok, unauthorized } from '../../../utils/cannedHTTPResponses'
+import { NextFunction, Response, Router } from 'express'
+import { ExpressHandlerRequest } from '../../../types/ExpressHandlerRequest'
+import { badRequest, internalServerError, ok, unauthorized } from '../../../utils/cannedHTTPResponses'
 
 const router = Router()
-
-// TODO: Auth checks
-
 declare module 'express-session' {
   interface SessionData {
     token: string
@@ -13,40 +11,42 @@ declare module 'express-session' {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/login', async (req, res, next) => {
+router.post('/login', async (req: ExpressHandlerRequest, res: Response, next: NextFunction): Promise<void> => {
+  logger.info(`Attempting login with username=${req.body.username} and password=${req.body.password}`)
   try {
     if (req.body.username === process.env.LOGIN_USER && req.body.password === process.env.LOGIN_PASS) {
       req.session.token = crypto.randomBytes(32).toString('hex')
       req.session.save()
-      logger.info('User authenticated. Login successful.')
+      logger.info(`User ${req.body.username} login successful.`)
       ok(res)
     } else {
-      logger.warn('User input invalid. Login unsuccessful.')
-      res.sendStatus(400)
+      logger.warn(`User input invalid (Username=${req.body.username}, password=${req.body.password}). Login unsuccessful.`)
+      badRequest(res)
     }
   } catch (err) {
-    logger.warn('Error during login.')
+    logger.warn(`An error occured during login: ${err.stack}`)
     internalServerError(res)
   }
 })
 
 router.post('/auth', (req, res, next) => {
+  logger.info(`Received a request to authenticate session ${req.session.token}`)
   try {
     if (req.session.token) {
-      logger.info('Token authentication successful.')
+      logger.info(`Session authentication with token ${req.session.token} successful.`)
       ok(res)
     } else {
-      logger.warn('Token authentication unsuccessful.')
+      logger.warn(`Session authentication unsuccessful. Invalid session token ${req.session.token}.`)
       unauthorized(res)
     }
-  } catch (error) {
-    logger.warn('Error during session authentication.')
+  } catch (err) {
+    logger.warn(`An error occured during session authentication ${err.stack}`)
     internalServerError(res)
   }
 })
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/logout', async (req, res, next) => {
+router.post('/logout', async (req: ExpressHandlerRequest, res: Response, next: NextFunction): Promise<void> => {
   logger.info(`Invalidating session ${req.session.token}`)
   req.session.destroy(() => {})
   ok(res)
