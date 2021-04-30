53a097602b5bc63072d3f15940cbe12345f872bb
Linus Willner <hello@linuswillner.me>
2021-04-30 20:22:33 +0300

    Improve HTTP request error logging

--- src/utils/logHTTPRequestError.ts
+++ src/utils/logHTTPRequestError.ts
@@ -1,3 +1,5 @@
+import { inspect } from 'util'
+
 export default (err: any): void => {
   logger.error('An HTTP request failed. More information to follow.')
 
@@ -8,21 +10,21 @@ export default (err: any): void => {
     const { data, status, headers } = err.response
 
     logger.error('Request succeeded, but response was in error range (4xx-5xx).')
-    logger.error(`Status: ${status}`)
-    logger.error(`Headers: ${headers}`)
-    logger.error(`Data: ${data}`)
+    logger.error(`Status: ${inspect(status)}`)
+    logger.error(`Headers: ${inspect(headers)}`)
+    logger.error(`Data: ${inspect(data)}`)
   } else if (err.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
 
     logger.error('Request was made, but no response was received.')
-    logger.error(`Request data: ${err.request}`)
+    logger.error(`Request data: ${inspect(err.request)}`)
   } else {
     logger.error('A generic error occurred, request could not be constructed.')
-    logger.error(`Error: ${err.message}`)
+    logger.error(`Error: ${inspect(err.message)}`)
   }
 
   logger.error(`Stack trace: ${err.stack}`)
-  logger.error(`Request configuration: ${err.config}`)
+  logger.error(`Request configuration: ${inspect(err.config)}`)
 }
