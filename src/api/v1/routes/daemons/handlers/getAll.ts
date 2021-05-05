14a5824a52aff6f027dd72570681a3d5ddffccd2
Linus Willner <hello@linuswillner.me>
2021-05-05 03:11:40 +0300

    Don't send unidentified daemons downstream

--- src/api/v1/routes/daemons/handlers/getAll.ts
+++ src/api/v1/routes/daemons/handlers/getAll.ts
@@ -8,10 +8,12 @@ import stripDaemon from '../../../../../utils/stripDaemon'
 export default async (req: ExpressHandlerRequest, res: Response, next: NextFunction): Promise<void> => {
   try {
     // Strip extraneous and sensitive data from daemons before sending it downstream
-    const daemons = (await dbDriver.getAll(true)).map((daemon: DaemonDocument) => {
-      const tempDaemon = daemon as unknown // TypeScript hack to downgrade DaemonDocument into a Daemon
-      return stripDaemon(tempDaemon as Daemon)
-    })
+    const daemons = (await dbDriver.getAll(true))
+      .filter((daemon: DaemonDocument) => daemon.ip) // Don't send daemons that have not yet identified
+      .map((daemon: DaemonDocument) => {
+        const tempDaemon = daemon as unknown // TypeScript hack to downgrade DaemonDocument into a Daemon
+        return stripDaemon(tempDaemon as Daemon)
+      })
 
     res.status(200).json(daemons)
   } catch (err) {
